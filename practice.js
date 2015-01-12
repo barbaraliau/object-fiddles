747400330d5c40d2551ce2c08d3f4b04478e2a2f
Barbara Liau <bj.liau@gmail.com>
2015-01-12 12:27:55 -0700

    Created backPack object, added keys and values, alert, console.log

--- practice.js
+++ practice.js
@@ -57,12 +57,16 @@ with the value being the color of your backpack. */
 
   //Code here
 
+var backPack = {};
+var item = "firstPocket";
+backPack["feature"] = item;
+backPack.color = "green";
 
 //After you do the above, alert your entire backPack object.
 
   //Code here
 
-
+alert(backPack);
 
 /*You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
@@ -70,7 +74,7 @@ Instead, console.log your whole backPack object and then check out the console. 
 
   //Code here
 
-
+console.log(backPack);
 
 
 //NEXT PROBLEM
